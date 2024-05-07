6f93964b1936963fd64c8ecbe9d1400f005b9974
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-05-07 17:04:41 -0300

    feat: Cria entidade usuário

---
+++ AuthApi/src/main/java/com/example/AuthApi/entities/UserEntity.java
@@ -0,0 +1,63 @@
+package com.example.AuthApi.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "GMDS_USER")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId(Long id) {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin(String login) {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail(String email) {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword(String password) {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
