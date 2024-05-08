2bde9498bff556fcecc92e121150d0f73adb4ea1
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-05-08 01:58:31 -0300

    feat: Cria dto para classe usuário

--- AuthApi/src/main/java/com/example/AuthApi/dtos/UserDto.java
+++ AuthApi/src/main/java/com/example/AuthApi/dtos/UserDto.java
@@ -1,2 +1,64 @@
-package com.example.AuthApi.dtos;public class UserDto {
+package com.example.AuthApi.dtos;
+
+import com.example.AuthApi.entities.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.BeanUtils;
+
+public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String login;
+
+    private String email;
+
+    private String password;
+
+    public UserDto(UserEntity user) {
+        BeanUtils.copyProperties(user, this);
+    }
+
+    public Long getId(Long id) { return this.id; }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin(String login) {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail(String email) {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword(String password) {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
