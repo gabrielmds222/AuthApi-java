e89fa2041e292c753cd30f8c4d05b9edd81737df
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-05-08 02:25:18 -0300

    feat: Adiciona repository do usuário

--- AuthApi/src/main/java/com/example/AuthApi/repositories/UserRepository.java
+++ AuthApi/src/main/java/com/example/AuthApi/repositories/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.AuthApi.repositories;public interface UserRepository {
+package com.example.AuthApi.repositories;
+
+import com.example.AuthApi.entities.UserEntity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
