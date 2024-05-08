2d7a11a15c40a0df6f43afd262235eb88bb75314
gabrielmedsilva <gabrielmedsilva@outlook.com>
2024-05-08 02:26:29 -0300

    feat: Adiciona os serviços:
    - Listar todos os usuários.
    - Adicionar usuários.
    - Editar usuário.
    - Deletar usuário.
    - Listar usuário pelo id.

--- AuthApi/src/main/java/com/example/AuthApi/services/UserService.java
+++ AuthApi/src/main/java/com/example/AuthApi/services/UserService.java
@@ -1,2 +1,40 @@
-package com.example.AuthApi.services;public class UserService {
+package com.example.AuthApi.services;
+
+import com.example.AuthApi.dtos.UserDto;
+import com.example.AuthApi.entities.UserEntity;
+import com.example.AuthApi.repositories.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDto> listAll() {
+        List<UserEntity> users = userRepository.findAll();
+        return users.stream().map(UserDto::new).toList();
+    }
+
+    public void insert(UserDto user) {
+        UserEntity userEntity = new UserEntity(user);
+        userRepository.save(userEntity);
+    }
+
+    public UserDto update(UserDto user) {
+        UserEntity userEntity = new UserEntity(user);
+        return new UserDto(userRepository.save(userEntity));
+    }
+
+    public void delete(Long id) {
+        UserEntity user = userRepository.findById(id).get();
+        userRepository.delete(user);
+    }
+
+    public UserDto searchById(Long id) {
+        return  new UserDto(userRepository.findById(id).get());
+    }
 }
